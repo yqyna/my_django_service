6f9da89ce77e4c76305354ddb5b5094f0a249c89
yuhaiping <y960754862@163.com>
2022-12-30 18:06:56 +0800

    ADD Use Minio SDK Example

--- apps/utils/example_test_file/minio_operation_eg/use_minio_example.py
+++ apps/utils/example_test_file/minio_operation_eg/use_minio_example.py
@@ -12,4 +12,17 @@ client = Minio(
     secure=False
 )
 
+# Download file
 client.fget_object('test', 'IMG_1162.MOV', 'IMG_1162.MOV')
+
+# 1.When don't set param content_type and use fput_object func to upload picture successful,
+# then use func presigned_get_object to get the url which can't preview in browser but download picture by browser
+client.fput_object('test', 'test1.jpeg', 'test1.jpeg')
+url = client.presigned_get_object('test', 'test1.jpeg')
+print(" url can't preview in browser:", url)
+
+# 2.When set param content_type and use fput_object func to upload picture successful,
+# then use func presigned_get_object to get the url which can preview in browser
+client.fput_object('test', 'test2.jpeg', 'test2.jpeg', content_type="image/jpeg")
+url = client.presigned_get_object('test', 'test2.jpeg')
+print("url can preview in browser:", url)
